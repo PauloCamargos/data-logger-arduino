7829a85b9189ce7bc5779847d12587c3e856166c
Ítalo Fernandes <italogsfernandes@gmail.com>
2018-05-09 13:49:54 -0300

    recepção do comando serial

--- Firmware/fake-data-acquirer/src/fake-data.ino
+++ Firmware/fake-data-acquirer/src/fake-data.ino
@@ -1,4 +1,11 @@
 
+#define CMD_LIGAR_VENTILADOR    'T'
+#define CMD_DESLIGAR_VENTILAR   't'
+#define CMD_LIGAR_IRRIGADOR     'U'
+#define CMD_DESLIGAR_IRRIGADOR  'u'
+
+#define PINO_VENTILADOR 13
+#define PINO_IRRIGADOR  12
 
 char cmd_serial;
 void setup(){
@@ -8,7 +15,20 @@ void setup(){
 void loop(){
     if(Serial.available()){
         cmd_serial = Serial.read();
-
+        switch(cmd_serial){
+            case CMD_LIGAR_VENTILADOR:
+            digitalWrite(PINO_VENTILADOR, HIGH);
+            break;
+            case CMD_DESLIGAR_VENTILADOR:
+            digitalWrite(PINO_VENTILADOR, LOW);
+            break;
+            case CMD_LIGAR_IRRIGADOR:
+            digitalWrite(PINO_IRRIGADOR, HIGH);
+            break;
+            case CMD_DESLIGAR_IRRIGADOR:
+            digitalWrite(PINO_IRRIGADOR, LOW);
+            break;
+        }
     }
 
 }
