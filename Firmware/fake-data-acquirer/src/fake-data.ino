074abb40e4b502d2ece72bf75e7350e0af380281
Ítalo Fernandes <italogsfernandes@gmail.com>
2018-05-09 16:28:26 -0300

    gerador de dados falso finalizado

--- Firmware/fake-data-acquirer/src/fake-data.ino
+++ Firmware/fake-data-acquirer/src/fake-data.ino
@@ -1,15 +1,38 @@
-
-#define CMD_LIGAR_VENTILADOR    'T'
-#define CMD_DESLIGAR_VENTILAR   't'
-#define CMD_LIGAR_IRRIGADOR     'U'
-#define CMD_DESLIGAR_IRRIGADOR  'u'
-
+//////////
+//Pinos //
+//////////
 #define PINO_VENTILADOR 13
 #define PINO_IRRIGADOR  12
 
+/////////////
+//Comandos //
+/////////////
+#define CMD_LIGAR_VENTILADOR    'T'
+#define CMD_DESLIGAR_VENTILAR   't'
+
+#define CMD_LIGAR_IRRIGADOR     'U'
+#define CMD_DESLIGAR_IRRIGADOR  'u'
+
+#define CMD_LER_DADOS           'R'
+
+////////////////
+//Comunicação //
+////////////////
+#define COMM_START          '$'
+#define COMM_UMIDADE        'U'
+#define COMM_TEMPERATURA    'T'
+#define COMM_END            '\n'
+
+////////////////
+//Global Data //
+////////////////
 char cmd_serial;
+float fake_temp, fake_umid;
+
 void setup(){
     Serial.begin(9600);
+    fake_temp = 15;
+    fake_umid = 10;
 }
 
 void loop(){
@@ -28,7 +51,23 @@ void loop(){
             case CMD_DESLIGAR_IRRIGADOR:
             digitalWrite(PINO_IRRIGADOR, LOW);
             break;
+            case CMD_LER_DADOS:
+            Serial.write(COMM_START);
+            Serial.write(COMM_TEMPERATURA);
+            Serial.print(fake_temp, 2);
+            Serial.write(COMM_END);
+            Serial.write(COMM_START);
+            Serial.write(COMM_UMIDADE);
+            Serial.print(fake_umid, 2);
+            Serial.write(COMM_END);
+            fake_temp += 2.5;
+            fake_umid += 7.5;
+            if(fake_umid > 90){
+                fake_umid = 10;
+            }
+            if(fake_temp > 50){
+                fake_temp = 15;
+            }
         }
     }
-
 }
